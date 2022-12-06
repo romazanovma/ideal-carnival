f4c540f2362f73277fe430bc210e4a695c908cb6
Максим Ромазанов <romazanov@MacBook-Air-Maksim.local>
2022-12-06 17:38:08 +0400

    bugfix

--- dup_remover.py
+++ dup_remover.py
@@ -14,17 +14,20 @@ def duplicate_remover(initial_list):
     dup_free_set = set()
     dup_free_list = []
     for element in initial_list:
-        str_element = json.dumps(element)
-        if str_element not in dup_free_set:
-            dup_free_set.add(str_element)
+        hashable_element = json.dumps(sorted(element))
+        if hashable_element not in dup_free_set:
+            dup_free_set.add(hashable_element)
             dup_free_list.append(element)
     return dup_free_list
 
+
 def main():
     duplicate_list = [{"key1": "value1"}, {"k1": "v1", "k2": "v2", "k3": "v3"},
-        {}, {}, {"key1": "value1"}, {"key1": "value1"}, {"key2": "value2"}]
+                      {"k2": "v2", "k1": "v1", "k3": "v3"}, {}, {}, {"key1": "value1"},
+                      {"key1": "value1"}, {"key2": "value2"}]
     dup_free_list = duplicate_remover(duplicate_list)
     print(f"Список без дубликатов: {dup_free_list}")
+
 
 if __name__ == "__main__":
     main()
